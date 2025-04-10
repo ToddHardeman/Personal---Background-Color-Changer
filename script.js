2d7c23763ecbef0fdbd485c1de89f471be7ed78f
Todd Hardeman <toddhardeman6@gmail.com>
2025-04-09 19:46:33 -0700

    updated the color change to now change the linear-gradient WHTIE only instead of updating the entire background color

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const body = document.body;
 
 function changeBackgroundColor() {
     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    body.style.backgroundcolor = randomColor;
+    body.style.backgroundImage = `linear-gradient(to right, black, ${randomColor})`;
 }
 button.addEventListener('click', changeBackgroundColor);
